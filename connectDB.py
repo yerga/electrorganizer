f9e312b2b123c02011d256a76df769f713d57fa3
yerga <dyerga@gmail.com>
2016-12-10 20:34:51 +0100

    Using TableView and QSqlTableModel. First SQL functionalization

---
+++ connectDB.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from PyQt5.QtWidgets import QMessageBox, qApp
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery
+
+def createDB():
+    db = QSqlDatabase.addDatabase('QSQLITE')
+    db.setDatabaseName('electrorganizer.db')
+
+    if not db.open():
+        QMessageBox.critical(None, qApp.tr("Cannot open database"),
+                                   qApp.tr("Unable to establish a database connection.\n"
+                                                 "This example needs SQLite support. Please read "
+                                                 "the Qt SQL driver documentation for information "
+                                                 "how to build it.\n\n" "Click Cancel to exit."),
+                                   QMessageBox.Cancel)
+
+        return False
+
+    query = QSqlQuery()
+    query.exec_("create table components(id int primary key, "
+                "name varchar(30), category varchar(20), description varchar(100), "
+                "amount int, manufacturer varchar(20), manid varchar(25), "
+                "supplier varchar(20), supid varchar(25), price float, storage varchar(20), "
+                "link varchar(100), datasheet varchar(100))")
+
+
+
+    query.exec_("insert into components values(1, '1N4001', 'Diodos', 'Rectificadores Vr/50V Io/1A', 10, 'Micro Commercial Components (MCC)', '1N4001-TP', 'Mouser', '833-1N4001-TP', 0.104, 'Caja blanca', 'http://www.mouser.es/ProductDetail/Micro-Commercial-Components-MCC/1N4001-TP', 'http://www.mouser.com/ds/2/258/1N4001-1N4007(DO-41)-349533.pdf')")
+    query.exec_("insert into components values(2, '2.2K', 'Resistencias', 'Resistores de película de metal - a través del orificio 1/4watt 2.2Kohms 5% Rated to 1/2watt', 30, 'Vishay / Dale', 'CCF072K20JKE36', 'Mouser', '71-CCF072K20JKE36', 0.094, 'Caja blanca', 'http://www.mouser.es/ProductDetail/Vishay-Dale/CCF072K20JKE36/', 'http://www.mouser.com/ds/2/427/ccf07-239748.pdf')")
+    query.exec_("insert into components values(3, 'name', 'cat', 'desc', 10, 'man', 'manid', 'supp', 'supid', 1.3, 'storage', 'link', 'data')")
+
+    return True
--- main.py
+++ main.py
@@ -9,17 +9,46 @@ Organizer for electronic components written in PyQt5
 author: Daniel Martin-Yerga (dyerga (at) gmail.com)
 """
 
+
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtSql import QSqlTableModel
+from PyQt5.QtCore import Qt
 from ui_mainwindow import Ui_MainWindow
+import connectDB
 
+
+def initializeModel(model):
+    model.setTable('components')
+    model.setEditStrategy(QSqlTableModel.OnFieldChange)
+    model.select()
+    model.setHeaderData(0, Qt.Horizontal, "ID")
+    model.setHeaderData(1, Qt.Horizontal, "Name")
+    model.setHeaderData(2, Qt.Horizontal, "Category")
+    model.setHeaderData(3, Qt.Horizontal, "Description")
+    model.setHeaderData(4, Qt.Horizontal, "Amount")
+    model.setHeaderData(5, Qt.Horizontal, "Manufacturer")
+    model.setHeaderData(6, Qt.Horizontal, "Man. ID")
+    model.setHeaderData(7, Qt.Horizontal, "Supplier")
+    model.setHeaderData(8, Qt.Horizontal, "Supp. ID")
+    model.setHeaderData(9, Qt.Horizontal, "Price")
+    model.setHeaderData(10, Qt.Horizontal, "Storage")
+    model.setHeaderData(11, Qt.Horizontal, "Link")
+    model.setHeaderData(12, Qt.Horizontal, "Datasheet")
 
 class MainWindow(QMainWindow, Ui_MainWindow):
-    def __init__(self):
+    def __init__(self, model):
         super(MainWindow, self).__init__()
         self.setupUi(self)
         self.actionExit.triggered.connect(self.quit)
         #self.addbtn.clicked.connect(self.addbtn_clicked)
+        self.tableView.setModel(model)
+        self.tableView.clicked.connect(self.findrow)
+        self.setWindowTitle("Electrorganizer")
+
+    def findrow(self, i):
+        selectedrow = i.row()
+        print ("selected row: ", selectedrow)
 
     def quit(self):
         app.quit()
@@ -28,7 +57,18 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = MainWindow()
+
+    if not connectDB.createDB():
+         sys.exit(1)
+
+    #button.clicked.connect(addrow)
+    #btn1.clicked.connect(lambda: model.removeRow(view1.currentIndex().row()))
+
+    model = QSqlTableModel()
+    selectedrow = -1
+    initializeModel(model)
+
+    window = MainWindow(model)
     window.show()
     sys.exit(app.exec_())
 
