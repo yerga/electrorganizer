9cb295b28a298a15f9b713b38a1901c915c8ec9d
yerga <dyerga@gmail.com>
2016-12-10 01:38:42 +0100

    Added some UI functionalization

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
-from PyQt4 import QtGui, QtCore, Qt
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Electrorganizer
+
+Organizer for electronic components written in PyQt5
+
+author: Daniel Martin-Yerga (dyerga (at) gmail.com)
+"""
+
 import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from ui_mainwindow import Ui_MainWindow
 
-def plot():
-    print ("Hello you!")
 
-def main():
-    app = QtGui.QApplication(sys.argv)
-    window = QtGui.QMainWindow()
-    button = QtGui.QPushButton("Hello, PyQt!")
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setupUi(self)
+        self.actionExit.triggered.connect(self.quit)
+        #self.addbtn.clicked.connect(self.addbtn_clicked)
 
-    button.connect(button, Qt.SIGNAL("clicked()"), plot)
+    def quit(self):
+        app.quit()
 
-    window.setCentralWidget(button)
-    window.show()
-    app.exec_()
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
