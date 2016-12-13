0a05298512998f20f168138dccc9a112e9fae99d
yerga <dyerga@gmail.com>
2016-12-14 00:14:46 +0100

    avoid recreating initial values

--- connectDB.py
+++ connectDB.py
@@ -27,8 +27,8 @@ def createDB():
 
 
 
-    query.exec_("insert into components values(1, '1N4001', 'Diodos', 'Rectificadores Vr/50V Io/1A', 10, 'Micro Commercial Components (MCC)', '1N4001-TP', 'Mouser', '833-1N4001-TP', 0.104, 'Caja blanca', 'http://www.mouser.es/ProductDetail/Micro-Commercial-Components-MCC/1N4001-TP', 'http://www.mouser.com/ds/2/258/1N4001-1N4007(DO-41)-349533.pdf')")
-    query.exec_("insert into components values(2, '2.2K', 'Resistencias', 'Resistores de película de metal - a través del orificio 1/4watt 2.2Kohms 5% Rated to 1/2watt', 30, 'Vishay / Dale', 'CCF072K20JKE36', 'Mouser', '71-CCF072K20JKE36', 0.094, 'Caja blanca', 'http://www.mouser.es/ProductDetail/Vishay-Dale/CCF072K20JKE36/', 'http://www.mouser.com/ds/2/427/ccf07-239748.pdf')")
-    query.exec_("insert into components values(3, 'name', 'cat', 'desc', 10, 'man', 'manid', 'supp', 'supid', 1.3, 'storage', 'link', 'data')")
+    #query.exec_("insert into components values(1, '1N4001', 'Diodos', 'Rectificadores Vr/50V Io/1A', 10, 'Micro Commercial Components (MCC)', '1N4001-TP', 'Mouser', '833-1N4001-TP', 0.104, 'Caja blanca', 'http://www.mouser.es/ProductDetail/Micro-Commercial-Components-MCC/1N4001-TP', 'http://www.mouser.com/ds/2/258/1N4001-1N4007(DO-41)-349533.pdf')")
+    #query.exec_("insert into components values(2, '2.2K', 'Resistencias', 'Resistores de película de metal - a través del orificio 1/4watt 2.2Kohms 5% Rated to 1/2watt', 30, 'Vishay / Dale', 'CCF072K20JKE36', 'Mouser', '71-CCF072K20JKE36', 0.094, 'Caja blanca', 'http://www.mouser.es/ProductDetail/Vishay-Dale/CCF072K20JKE36/', 'http://www.mouser.com/ds/2/427/ccf07-239748.pdf')")
+    #query.exec_("insert into components values(3, 'name', 'cat', 'desc', 10, 'man', 'manid', 'supp', 'supid', 1.3, 'storage', 'link', 'data')")
 
     return True
